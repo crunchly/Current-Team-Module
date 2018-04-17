1b9be3397f5b80ada4d3046baf1560ba582d2c61
Rosarlin Thai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-17 00:23:58 -0700

    Build skeleton

---
+++ client/src/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import CurrentTeam from '../client/src/components/CurrentTeam.jsx'
+
+ReactDOM.render(<CurrentTeam/>, document.getElementById('app'));
