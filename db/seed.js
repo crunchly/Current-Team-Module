c203f27d1305cd546e4ff1401e59c2111cf1dbc4
Rosarlin Thai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-17 15:51:00 -0700

    Remove comments

--- db/seed.js
+++ db/seed.js
@@ -1,10 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const mongoose = require('mongoose');
 
 const db = require('./index.js');
 const Person = require('./Person.js');
-
 
 //images are saved on s3 as files labeled 1-33
 const generateRandomImage = function() {
@@ -13,19 +11,10 @@ const generateRandomImage = function() {
   return Math.floor(Math.random() * (max - min + 1)) + min; 
 };
 
-// const insertPeople = function(people) {
-//   Person.create(people)
-//     .then(() => {
-//       console.log('Database seeded')
-//       db.disconnect();
-//     });
-// };
-
 fs.readFile(path.join(__dirname, 'people.json'), (error, data) => {
   if (error) return console.error('Error reading file: ', error);
 
   data = JSON.parse(data);
-  var people = [];
 
   data.forEach( person => {
     person.member_id = person.id;
@@ -36,17 +25,14 @@ fs.readFile(path.join(__dirname, 'people.json'), (error, data) => {
     delete person.affiliation_name;
     delete person.modified_at;
 
-    var member = new Person(person)
+    var member = new Person(person);
     member.save( (error, member) => {
       if (error) return console.error('Error saving to database: ', error);
-    })
-    // people.push(person);
-  });
-  // console.log(people); // people array looks good
-  // insertPeople(people); //but WONT SEED
-  console.log('database seeded');
+    });
 
+  });
 });
+
 
 
 
