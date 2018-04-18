3214d8fbfa5485f2130f076a850b0809fa479ea1
Rosarlin Thai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-18 15:39:02 -0700

    modify from find to aggregate

--- server/index.js
+++ server/index.js
@@ -1,4 +1,3 @@
-const bodyParser = require('body-parser');
 const express = require('express');
 const path = require('path');
 
@@ -6,33 +5,30 @@ const Person = require('../db/Person.js');
 
 const app = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/people/:org', (req, res) => {
   const company = req.params.org;
 
   // company query is case sensitve
-    // time permitting, should have set all company names as lowercase before seeding into db
-    // then query lowercase name
+  //   time permitting, should have set all company names as lowercase before seeding into db
+  //   then query lowercase name
 
-  Person.find({ company }).sort({ updated_at: -1 })
-    .then((result) => {
-      const members = [];
-      const team = [];
-
-      // if a person updated their title multiple times,
-      // there are duplicate entries of that person, and we only want their most recent update
-      result.forEach((row) => {
-        if (!members.includes(row.member_id)) {
-          members.push(row.member_id);
-          team.push(row);
-        }
-      });
-      res.send(team);
-    });
+  // if a person updated their title multiple times,
+  // there are duplicate entries of that person, and we only want their most recent update
+  Person.aggregate([
+    { $match: { company } },
+    { $sort: { updated_at: -1 } },
+    { $group: {
+        _id : '$member_id',
+        first_name: {$first: '$first_name'},
+        last_name: {$first: '$last_name'}, 
+        title: {$first: '$title'}
+      }
+    },
+    { $sort: { _id: 1 } },
+  ])
+  .then(team => res.send(team));
 });
 
 app.listen(3004, () => console.log('Listening on port 3004...'));
