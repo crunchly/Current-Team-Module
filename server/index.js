b3d8767c7e67d4acba6add7f5f0745ed73872364
Rosarlin Thai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-17 23:33:35 -0700

    build express server

--- server/index.js
+++ server/index.js
@@ -1,27 +1,29 @@
 const bodyParser = require('body-parser');
 const express = require('express');
+const path = require('path');
 
-const Person = require('/../db/Person.js')
+const Person = require('../db/Person.js');
 
 const app = express();
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use(express.static(__dirname, '/../client/dist'));
+app.use(express.static(path.join(__dirname, '../client/dist')));
 
 app.get('/people/:org', (req, res) => {
-  var company = req.params.org;
+  const company = req.params.org;
 
-  //company query is case sensitve, time permitting
-    //should have seeded company name as lowercase and queried that instead
+  // company query is case sensitve
+    // time permitting, should have set all company names as lowercase before seeding into db
+    // then query lowercase name
 
-  Person.find({company: company}).sort({updated_at: -1})
-    .then( result => {
-      //do something with result
-      var members = [];
-      var team = [];
-      result.forEach( (row) => {
+  Person.find({ company }).sort({ updated_at: -1 })
+    .then((result) => {
+      const members = [];
+      const team = [];
+
+      result.forEach((row) => {
         if (!members.includes(row.member_id)) {
           members.push(row.member_id);
           team.push(row);
@@ -31,4 +33,4 @@ app.get('/people/:org', (req, res) => {
     });
 });
 
-app.listen(3004, () => console.log('Listening on port 3004...'))
+app.listen(3004, () => console.log('Listening on port 3004...'));
