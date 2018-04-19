9f838da3b9b6709c1f6fd9c69c9cb5a8f23d1b32
Rosarlin Thai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-18 18:06:40 -0700

    update rendering

--- client/src/components/TeamMember.jsx
+++ client/src/components/TeamMember.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
 const TeamMember = (props) => (
-  <div class="member-info">
-    <span>image</span>
+  <div className="member-info">
+    <img src={props.person.photo} alt={props.person.first_name + ' ' + props.person.last_name}></img>
     <ul>
-      <li><a href="#">{props.person.first_name + ' '}{props.person.last_name}</a></li>
+      <li><a href="#">{props.person.first_name + ' ' + props.person.last_name}</a></li>
       <li>{props.person.title}</li>
     </ul>
   </div>
