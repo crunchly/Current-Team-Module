e554fc586d3b64ceadf798623c9124175eac8890
rthai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-22 20:35:03 -0700

    add wrapping divs

--- client/src/components/TeamMember.jsx
+++ client/src/components/TeamMember.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 
 const TeamMember = (props) => (
   <div className="team-member">
-    <img src={props.person.photo_url} alt={props.person.first_name + ' ' + props.person.last_name}></img>
+    <div className="team-member-img-wrap">
+      <img src={props.person.photo_url} alt={props.person.first_name + ' ' + props.person.last_name}></img>
+    </div>
     <div className="team-member-info">
       <ul>
         <li><a className="member-link" href="#">{props.person.first_name + ' ' + props.person.last_name}</a></li>
