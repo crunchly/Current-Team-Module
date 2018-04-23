5246c2ce783b72a69132f8bb3e46ceb5a6ca03cf
rthai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-22 20:42:29 -0700

    change total number into a link

--- client/src/components/TeamTotal.jsx
+++ client/src/components/TeamTotal.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const TeamTotal = (props) => (
   <div className="team-total-container">
     <div className="team-total">
-      <h3>Number of Current Team <span>Members</span> <span>{' ' + props.total}</span></h3>
+      <h3>Number of Current Team <span>Members</span> <a href="#">{props.total}</a></h3>
     </div>
   </div>
 );
