980328ac8f06ba92e26ae4b49a0279a35a35d48e
rthai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-24 14:48:38 -0700

    remove renderer, add org prop, passes test

--- __tests__/CurrentTeam.test.jsx
+++ __tests__/CurrentTeam.test.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Enzyme from 'enzyme';
 import {shallow, mount, render} from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import renderer from 'react-test-renderer';
-
 import CurrentTeam from '../client/src/components/CurrentTeam.jsx';
 
 Enzyme.configure({ adapter: new Adapter() });
@@ -11,7 +9,7 @@ Enzyme.configure({ adapter: new Adapter() });
 describe('CurrentTeam Component', () => {
 
   test('CurrentTeam component should exist', () => {
-    const wrapper = shallow(<CurrentTeam />);
+    const wrapper = shallow(<CurrentTeam org={'Facebook'}/>);
 
     expect(CurrentTeam).toBeDefined();
   });
