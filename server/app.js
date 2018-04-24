4cb25ac68062a0f4fcd2af11ba8f0a98ebc5aabf
rthai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-23 21:50:15 -0700

    add getTeam back to server

--- server/app.js
+++ server/app.js
@@ -16,20 +16,8 @@ app.get('/people/:org', (req, res) => {
 
   // if a person updated their title multiple times,
   // there are duplicate entries of that person, and we only want their most recent update
-  Person.aggregate([
-    { $match: { company } },
-    { $sort: { updated_at: -1 } },
-    { $group: {
-        _id : '$member_id',
-        first_name: {$first: '$first_name'},
-        last_name: {$first: '$last_name'}, 
-        title: {$first: '$title'},
-        photo_url: {$first: '$thumbnail_url'},
-      }
-    },
-    { $sort: { _id: 1 } },
-  ])
-  .then(team => res.send(team));
+  Person.getTeam(company)
+    .then(team => res.send(team));
 });
 
 module.exports = app;
