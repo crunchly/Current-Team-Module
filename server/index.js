bcfe1e374b1cfb104e6c2eefa6bed8b9e991c3e4
rthai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-23 21:27:58 -0700

    split server/index.js into app

--- server/index.js
+++ server/index.js
@@ -1,17 +1,3 @@
-const express = require('express');
-const path = require('path');
-
-const Person = require('../db/Person.js');
-
-const app = express();
-
-app.use(express.static(path.join(__dirname, '../client/dist')));
-
-app.get('/people/:org', (req, res) => {
-  const company = req.params.org;
-
-  Person.getTeam(company)
-    .then(team => res.send(team));
-});
+const app = require('./app.js');
 
 app.listen(3004, () => console.log('Listening on port 3004...'));
