9d6e79296a5e4821023952b3b1c4cdf2d7b194c6
rthai <rosarlin@rosarlins-MacBook-Pro.local>
2018-04-23 21:27:04 -0700

    add express server test

---
+++ __tests__/app.test.js
@@ -0,0 +1,21 @@
+const request = require('supertest');
+const app = require('../server/app.js');
+
+describe('Test the root path', () => {
+  test('It should respond to GET method', () => {
+    return request(app)
+      .get('/')
+      .then(response => {
+        expect(response.statusCode).toBe(200)
+      })
+  });
+
+  test('It should respond to GET method', () => {
+    return request(app)
+      .get('/people/Facebook')
+      .then(response => {
+        expect(response).toHaveLength(18);
+      })
+  });
+
+})
